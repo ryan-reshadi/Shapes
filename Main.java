7443870ac9359e136bde6c6dc016fdb3df8d456d
ryan-reshadi <ryan@reshadi.com>
2025-01-03 16:13:43 -0800

    Added Code to create JFrame; Added Point class
    - added code to initialize JFrame
    - added Point class
    - added color attribute to Shape class

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 package Shapes;
 
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int w = 800;
+        int h = 600;
+        JFrame f = new JFrame();
+        Shape[] shapeArray = {};
+        DrawingCanvas dc = new DrawingCanvas(w, h, shapeArray);
+        f.setSize(w, h);
+        f.setTitle("Canvas");
+        f.add(dc);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
     }
 }
--- Shape.java
+++ Shape.java
@@ -1,6 +1,7 @@
 package Shapes;
 
 public class Shape {
+    String color;
     public void draw(){
         System.out.println("Derive from Shape");
     }
